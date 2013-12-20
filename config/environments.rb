0445895774f7f654560f05a9f289347debc64643
Ethan Schmertzler <ethanschmertzler@mac.com>
2013-12-20 15:00:10 -0500

    Added database setting

--- config/environments.rb
+++ config/environments.rb
@@ -8,3 +8,17 @@ configure :development do
     encoding: "utf8",
   )
 end
+
+configure :production do
+  # HEROKU CONFIG
+  db = URI.parse(ENV['HEROKU_POSTGRESQL_YELLOW_URL '])
+  #configuration info
+  ActiveRecord::Base.establish_connection(
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host    => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+  )
+end
